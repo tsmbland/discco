6c7896e887cb3826c0ecd808082c754fddbfa9d6
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2023-01-24 11:12:43 +0000

    added to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='discco',
-    version='0.1.0',
+    version='0.2.0',
     license="CC BY 4.0",
     author='Tom Bland',
     author_email='tom_bland@hotmail.co.uk',

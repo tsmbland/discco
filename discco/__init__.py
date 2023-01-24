65bd4c07b64691dfdee3cfee02c5fcf22c5ee621
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2023-01-24 10:53:20 +0000

    initial commit

---
+++ discco/__init__.py
@@ -0,0 +1,1 @@
+from .discco import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='discco',
+    version='0.1.0',
+    license="CC BY 4.0",
+    author='Tom Bland',
+    author_email='tom_bland@hotmail.co.uk',
+    packages=find_packages(),
+    install_requires=[]
+)

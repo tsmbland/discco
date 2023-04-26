a2d7005a53a9d94ec2d42fb5b782c5516cf9975a
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2023-04-26 15:34:44 +0100

    added github url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='discco',
-    version='0.2.3',
+    version='0.2.4',
     license="CC BY 4.0",
     author='Tom Bland',
     author_email='tom_bland@hotmail.co.uk',
@@ -21,5 +21,8 @@ setup(
                       'par-segmentation'],
     description='Quantification of membrane and cytoplasmic concentrations based on differentiable simulation of cell cortex images',
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    project_urls={
+        "Source Code": "https://github.com/tsmbland/discco",
+    }
 )
